ac767d7c937409197e9387d3ec3613f433674c47
jaypee1414 <jhnplbncyrn03@gmail.com>
2024-09-16 20:05:24 +0800

    add: mobile responsiveness car listing

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -12,11 +12,11 @@ function CarList() {
   return (
     <div>
       <h2 className="font-bold text-3xl text-center mt-10">Most Searched Car List</h2>
-        <ul className="mx-28">
+        <ul className="mx-12 md:mx-28">
         <Carousel>
              <CarouselContent>
                 {CarListFaker.carList.map((Car , index) => (
-                <CarouselItem className="basis-1/4"><CarCard key={index} Car={Car}/></CarouselItem>
+                <CarouselItem className="basis-1/2 md:basis-1/4"><CarCard key={index} Car={Car}/></CarouselItem>
                 ))}
             </CarouselContent>
             <CarouselPrevious />
--- src/components/ui/CarCard.jsx
+++ src/components/ui/CarCard.jsx
@@ -9,25 +9,25 @@ function CarCard({ Car }) {
     <div className=' hover:shadow-md cursor-pointer rounded-b-xl'>
       <img src={Car.image} alt="" width={300} height={250} className='rounded-t-xl mt-10'/>
       <div className='p-4'>
-        <h2 className='font-bold text-lg mb-2'>{Car.name}</h2>
+        <h2 className='font-bold text-sm md:text-lg mb-2'>{Car.name}</h2>
         <Separator className='border-2'/>
-        <div className='grid grid-cols-3 mt-5 mb-5'>
+        <div className='grid grid-rows-3 md:grid-cols-3 mt-5 mb-5'>
             <div className="flex flex-col item-center">
-                <TbManualGearboxFilled className='text-2xl'/>   
+                <TbManualGearboxFilled className='text-xl md:text-2xl'/>   
                 <h4>{Car.gearType}</h4>
             </div>
             <div className="flex flex-col item-center">
-                <IoLogoModelS className='text-2xl'/>
+                <IoLogoModelS className='text-xl md:text-2xl'/>
                 <h4>{Car.model}</h4>
             </div>
             <div className="flex flex-col item-center">
-                <BsFuelPumpDieselFill  className='text-2xl'/>
+                <BsFuelPumpDieselFill  className='text-xl md:text-2xl'/>
                 <h4>{Car.fuel}</h4>
             </div>
         </div>
         <Separator className='border-2'/>
         <div className='flex flex-row justify-between my-5'>
-            <h4 className='font-bold text-lg'>₱{Car.price} / hour</h4>
+            <h4 className='font-bold tex-md md:text-lg'>₱{Car.price} / hour</h4>
             <h2 className='text-indigo-600 text-sm flex gap-2 items-center cursor-pointer hover:shadow-sm'><MdOpenInNew /> View Details</h2>
         </div>
       </div>
