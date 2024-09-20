f86217ab0b897dd675c7c3a2788e38357c80bfe3
jaypee1414 <jhnplbncyrn03@gmail.com>
2024-09-20 18:06:07 +0800

    add: setup schema

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,6 +1,6 @@
 /** @type { import("drizzle-kit").Config } */
 export default {
-    schema: "./schema.ts",
+    schema: "./configs/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
       url: 'postgresql://cardb_owner:Ca3Wdp7IUGSM@ep-lucky-breeze-a56w6evu.us-east-2.aws.neon.tech/cardb?sslmode=require'
