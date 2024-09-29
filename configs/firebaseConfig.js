8be616a8f97bc0a6833fe06dce9751e67c26a340
jaypee1414 <jhnplbncyrn03@gmail.com>
2024-09-29 15:15:03 +0800

    add: firebase storage config

---
+++ configs/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getStorage} from "firebase/storage"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_KEY,
+  authDomain: "car-maketplace.firebaseapp.com",
+  projectId: "car-maketplace",
+  storageBucket: "car-maketplace.appspot.com",
+  messagingSenderId: "129511452315",
+  appId: "1:129511452315:web:429212e06656daf310a03e",
+  measurementId: "G-5GCFB0Z82B"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app)
