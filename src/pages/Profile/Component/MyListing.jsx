08a2c9aa4c82a4cdf6a70ccf5c3c7e216875b42a
jaypee1414 <jhnplbncyrn03@gmail.com>
2024-10-03 22:01:34 +0800

    add getCarlisting function base on the user account

--- src/pages/Profile/Component/MyListing.jsx
+++ src/pages/Profile/Component/MyListing.jsx
@@ -4,19 +4,23 @@ import { Link } from 'react-router-dom'
 import { db } from '../../../../configs/index'
 import { carImages, carListing } from '../../../../configs/schema'
 import { desc, eq } from 'drizzle-orm'
+import { useUser } from '@clerk/clerk-react'
 
 function MyListing() {
+
+    const {user} = useUser()
 
     useEffect(()=>{
         const getCarListing = async () =>{
             const result = await db.select()
             .from(carListing)
             .leftJoin(carImages,eq(carListing.id, carImages.carListingId))
+            .where(eq(carListing.createdBy, user?.primaryEmailAddress?.emailAddress))
             .orderBy(desc(carListing.id))
             console.log(result)
         }
         getCarListing()
-    },[])
+    },[user])
   return (
     <div>
         <h2 className="font-bold text-4xl">My Listing</h2>
