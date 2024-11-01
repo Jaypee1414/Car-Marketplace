a818bfd9ddeb52bba6a4418fc51ead92674dd9a4
jaypee1414 <jhnplbncyrn03@gmail.com>
2024-11-01 08:58:11 +0800

    refactor: add view function on browse by type

--- src/search/[category]/index.jsx
+++ src/search/[category]/index.jsx
@@ -7,6 +7,14 @@ import { db } from "../../../configs/index";
 import { carImages, carListing } from "../../../configs/schema";
 import { eq } from "drizzle-orm";
 import Service from "@/shared/Service";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import CarCard from "@/components/ui/CarCard"
 
 
 function SearchCategory() {
@@ -42,13 +50,19 @@ function SearchCategory() {
             <h2 className="md:text-3xl font-semibold flex justify-center">
                 List of {category}
             </h2>
-            {carDetails.map((car)=>{
-                return(
-                    <div>
-                        {car.category}
-                    </div>
-                )
-            })}
+          <ul className="mx-12 md:mx-28">
+            <Carousel>
+                <CarouselContent>
+                    {carDetails.map((Car , index) => (
+                    <CarouselItem className="basis-1/2 md:basis-1/4" key={index}>
+                      <CarCard Car={Car}/>
+                    </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+          </ul>
       </div>
 
     </div>
